4cf837cb66d6413cb707647106df7d4b023c41f9
bjh <baijianhua@gmail.com>
2020-01-02 10:08:42 +0800

    minor

--- metricsTensor.py
+++ metricsTensor.py
@@ -20,6 +20,7 @@ G = np.array([[1.5, 1],
 print("斜角坐标系到笛卡尔坐标系的变换矩阵(列是斜角坐标系的两个基向量笛卡尔读数)\n", G)
 metricsTensor = np.array([[g1 @ g1, g1 @ g2],
                           [g2 @ g1, g2 @ g2]])
+
 print("度量张量的第一种求法：基向量点乘\n", metricsTensor)
 metricsTensor = G.T @ G
 print("度量张量的第二种求法：G的转置矩阵乘以G\n", metricsTensor)
@@ -37,7 +38,13 @@ print("------------------------------\n")
 print("度规张量的最原始解释: 将向量的斜角坐标转换成笛卡尔坐标再运算，"
       "(Va @ G.T) @ (G @ Va)\n",
       (Va @ G.T) @ (G @ Va))
+
+# print("度规张量的最原始解释: 将向量的斜角坐标转换成笛卡尔坐标再运算，"
+#       "(Va @ G.T) @ (G @ Va)\n",
+#       np.matmul( np.matmul(Va, G.T), np.matmul(G, Va)))
+
 print("------------------------------\n")
 print("笛卡尔点乘Ve@Ve\n", Ve @ Ve)
+
 print("对偶点乘Vb@Va Va@Vb\n", Vb @ Va, Va @ Vb)
 print("用度量张量求长度\n", Va @ metricsTensor @ Va)
