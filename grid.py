dc0fc77df8ac2aeb5776fbc5fb23e09c7cec8571
baijinahua <18566300@qq.com>
2020-01-07 22:32:11 +0800

    vectors

--- grid.py
+++ grid.py
@@ -14,11 +14,14 @@ plt.grid(True)
 
 draw_vector(0,0,1,5)
 draw_vector(0, 0, 4, 0.5)
+# 绘制一个向量，并且绘制它到它的基向量、对偶基向量的平行四边形分解
+# 求一个向量在某个对偶坐标系的分量其实也容易，只要用这个向量与基向量点乘应该就可以了。
+draw_vector(0,0,6,6)
 # axes: Axes = plt.axes(label="axes1")
 # 获取当前坐标轴
 axes: Axes = plt.gca()
 axes.set_aspect('equal')
-axes.set_xlim(-2, 10)
-axes.set_ylim(-2, 10)
+axes.set_xlim(-5, 20)
+axes.set_ylim(-5, 20)
 
 plt.show()
