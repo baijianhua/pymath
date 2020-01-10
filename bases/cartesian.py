8a14e27bb9f998a08a94b789498ea7f32c2fac25
baijinahua <18566300@qq.com>
2020-01-10 23:52:59 +0800

    better.

--- bases/cartesian.py
+++ bases/cartesian.py
@@ -48,12 +48,12 @@ def draw_basis(ax: Axes, G: np.matrix, color="black"):
     #         color=color
     #         )
     max_scale = 10
-    g1 = [G[0, 0], G[1, 0]]
-    g2 = [G[0, 1], G[1, 1]]
+    g1 = get_column_from_matrix(G, 0)
+    g2 = get_column_from_matrix(G, 1)
 
     for i in range(max_scale):
-        ax.scatter(g1[0]*i, g1[1]*i, color="blue")
-        ax.scatter(g2[0]*i, g2[1]*i, color="blue")
+        ax.scatter(g1[0] * i, g1[1] * i, color="blue")
+        ax.scatter(g2[0] * i, g2[1] * i, color="blue")
 
     ax.plot([0, g1[0] * max_scale],
             [0, g1[1] * max_scale],
@@ -75,7 +75,31 @@ def draw_vector(ax: Axes, point: [], with_components=False):
 
 
 def draw_components(ax: Axes, point: []):
-    ax.plot([0, point[0]],
-            [point[0], point[1]], color="black", linewidth=2, linestyle="dashed")
-    ax.plot([point[1], point[0]],
-            [0, point[1]], color="black", linewidth=2, linestyle="dashed")
+    # 从表示向量的点，分别向两个坐标轴绘制虚线
+    ax.plot([point[0], point[0]],
+            [point[1], 0], color="black", linestyle="dashed")
+    ax.plot([point[0], 0],
+            [point[1], point[1]], color="black", linestyle="dashed")
+
+
+def get_column_from_matrix(m: np.mat, c: int) -> np.array:
+    return np.array([m[0, c], m[1, c]])
+
+
+def draw_components_in_coord(ax: Axes, v: np.array, G: np.mat, color="black"):
+    g1 = get_column_from_matrix(G, 0)
+    g2 = get_column_from_matrix(G, 1)
+    # x*g1, 即x个g1, 可以得到斜角坐标系的x分量，在笛卡尔坐标系的读数，即g1v.
+    g1v = g1 * v[0]
+    g2v = g2 * v[1]
+    # 从基向量的分量的笛卡尔坐标点，朝着向量所在点绘制虚线
+    ax.plot([g1v[0], v[0]],
+            [g1v[1], v[1]],
+            color=color,
+            linestyle="dashed"
+            )
+    ax.plot([g2v[0], v[0]],
+            [g2v[1], v[1]],
+            color=color,
+            linestyle="dashed"
+            )
--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 # https://matplotlib.org/3.1.1/tutorials/toolkits/axisartist.html
 import matplotlib.pyplot as plt
-from bases.cartesian import init_coord, draw_basis, draw_components, draw_vector
+from bases.cartesian import init_coord, draw_basis, draw_components, draw_vector, get_column_from_matrix, \
+        draw_components_in_coord
 from numpy import *
 
 ax = init_coord(-2, 6)
 # 用numpy的array而非python原生array,来方便做向量与标量的乘法，否则很麻烦
 g1 = array([1.3, 0.2])
 g2 = array([0.2, 1.5])
-# 协变分量的两个基向量为列构成的矩阵
+# 协变分量的两个基向量为列构成的矩阵,要转置一下，否则方向不对
 G = mat([g1, g2]).T
 print(G)
 # 逆变坐标系, 要是直接能用1/g1得出rg1就好了。这是不行的，不仅要g1*rg1=1, 还要g1*rg2=0
 G1 = G.I
 print(G1)
-rg1 = array([G1[0, 0], G1[1, 0]])
-rg2 = array([G1[0, 1], G1[1, 1]])
+rg1 = array(get_column_from_matrix(G1, 0))
+rg2 = array(get_column_from_matrix(G1, 1))
 print(rg1*3)
 print(rg2*3)
 
@@ -22,32 +23,10 @@ draw_basis(ax, G, "red")
 draw_basis(ax, G1, "blue")
 
 # 接下来如何显示向量以及它在不同坐标系下面的分量？
-draw_vector(ax, [3, 3], with_components=True)
-g13 = g1*3
-g23 = g2*3
-ax.plot([g13[0], 3],
-        [g13[1], 3],
-        color="red",
-        linestyle="dashed"
-        )
-ax.plot([g23[0], 3],
-        [g23[1], 3],
-        color="red",
-        linestyle="dashed"
-        )
-
-rg13 = rg1*3
-ax.plot([rg13[0], 3],
-        [rg13[1], 3],
-        color="blue",
-        linestyle="dashed"
-        )
-rg23 = rg2*3
-ax.plot([rg23[0], 3],
-        [rg23[1], 3],
-        color="blue",
-        linestyle="dashed"
-        )
+vector = array([2, 3])
+draw_vector(ax, vector, with_components=True)
+draw_components_in_coord(ax, vector, G, color="red")
+draw_components_in_coord(ax, vector, G1, color="blue")
 
 plt.show()
 
