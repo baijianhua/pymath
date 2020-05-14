45bba5a134b023acae250cad636df78b48c9a688
baijinahua <18566300@qq.com>
2020-05-14 20:32:02 +0800

    evalf

---
+++ christoffel/matrix_eval.py
@@ -0,0 +1,20 @@
+from sympy import *
+
+r, theta, a, b = symbols('r, theta, a, b')
+fx = r * sin(theta)
+fy = r * cos(theta)
+
+er_x = diff(fx, r)
+er_y = diff(fy, r)
+et_x = diff(fx, theta)
+et_y = diff(fy, theta)
+
+print_latex(er_x.subs({r: 3, theta: pi/6}).evalf())
+
+m = Matrix([
+    [er_x, et_x],
+    [et_x, et_y]
+]).subs({r: 3, theta: pi/6}).evalf()
+v = Matrix([a, b])
+cv = MatMul(m, v)
+print_latex(cv.subs({r: 3, theta: pi/6, a: 3, b: 2}).doit().evalf())
