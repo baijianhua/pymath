b5560e22db374624235e447287d7ee31238bf4f9
baijinahua <18566300@qq.com>
2020-06-07 15:13:40 +0800

    exp

--- special_relativity/Lorentz.py
+++ special_relativity/Lorentz.py
@@ -1,6 +1,10 @@
 from sympy import *
 
+# sympy化简 https://blog.csdn.net/FYZDMMCpp/article/details/86611948
+
+# 静止坐标系
 x, y, z, t, c = symbols("x,y,z,t,c")
+# 运动坐标系
 x1, y1, z1, t1 = symbols("x',y',z',t'")
 gamma, v = symbols('gamma, v')
 eq1 = Eq(x, gamma * (x1 + v * t1))
@@ -10,14 +14,22 @@ eq2 = Eq(x1, gamma * (x - v * t))
 # x1 的表达式，用x,v,t表示
 t1_exp = solve(eq1, t1)[0]
 x1_exp = solve(eq2, x1)[0]
+
 # x 的表达式，用x1,v,t1表示
 x_exp = solve(eq1, x)[0]
-# 将t1用x1表示
-t1_exp_subs = t1_exp.subs(x1, eq2.rhs).simplify()
+# x*x', 并代入x = ct, x'=ct'
 x_x1_exp = (x_exp * x1_exp).subs({x: c * t, x1: c * t1})
-print("x*x1=")
-print_latex(x_x1_exp)
+# 列方程
 eq3 = Eq(c * t * c * t1, x_x1_exp)
-gamma_exp = solve(eq3, gamma)[0].simplify()
-print_latex(gamma_exp)
-pprint(gamma_exp)
+gamma_exps = solve(eq3, gamma)
+# 求 gamma的值，因为gamma开平方，所以取[1]，正值，忽略负值
+gamma_exp = gamma_exps[1]
+# print_latex(gamma_exps)
+pprint.pformat(gamma, "=", gamma_exp)
+pprint(cancel(gamma_exp))
+
+
+# 将gamma带回t1的表达式
+t1_final = solve(Eq(t1_exp.subs({gamma: gamma_exp, x: c*t, x1: c*t1}), t1), t1)
+pprint(t1_final)
+
--- special_relativity/triangle.py
+++ special_relativity/triangle.py
@@ -1,9 +1,35 @@
 from sympy import *
 
-a, b, theta, c, t, t1, v = symbols('a b theta c t t1 v')
-eq = Eq(pow(c*t1*cos(theta), 2) + pow(v*t, 2), pow(c*t, 2))
-print_latex(solve(eq, t)[0].subs(theta, pi/6).simplify())
-print_latex(solve(eq, t1)[0].subs(theta, pi/6).simplify())
+
+# t1 静止坐标系的时间
+# t 运动坐标系的时间
+
+def square(num):
+    return num * num
+
+
+theta, c, v = symbols('theta c v')
+t = Symbol('t', nonnegative=true)
+t1 = Symbol('t1', nonnegative=true)
+
+va = 0
+eq = Eq(square(c * t1), square(c * t * cos(theta)) + square(v * t1 + c * t * sin(theta)))
+print_latex(solveset(eq, t))
+print_latex(solveset(eq, t1))
+
+print("-----------------")
+
+print_latex(solve(eq, t)[0].subs(theta, va).simplify())
+print_latex(solve(eq, t1)[0].subs(theta, va).simplify())
+
+print("cos=", cos(va), "sin=", sin(va))
+
+eq1 = Eq(square(c * t) + square(v * t1), square(c * t1))
+print_latex(eq1)
+print_latex(solveset(eq1, t))
+print_latex(solveset(eq1, t1))
+
+
 """
 到底想要求什么东西？是想要求运动者的一秒是静止者的多久，或者反过来
 那么一秒是怎么定义的？是光通过特定距离的时间。
